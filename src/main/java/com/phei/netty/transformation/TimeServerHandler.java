249b44871f51e1a2b4028d7bebc966347cc5e394
wayne <v-wachen@expedia.com>
2019-09-25 09:25:38 +0800

    use DelimiterBasedFrameDecoder to handle sticky bag issue

---
+++ src/main/java/com/phei/netty/transformation/TimeServerHandler.java
@@ -0,0 +1,39 @@
+package com.phei.netty.transformation;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Date;
+
+public class TimeServerHandler extends ChannelHandlerAdapter {
+
+    private int counter;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws UnsupportedEncodingException {
+        ByteBuf buf = (ByteBuf) msg;
+        byte[] req = new byte[buf.readableBytes()];
+        buf.readBytes(req);
+        String body = new String(req, "UTF-8")
+                .substring(0, req.length - System.getProperty("line.separator").length());
+        System.out.println("The time server receives order: " + body + " ; the counter is: " + ++counter);
+        String currentTime = "Query time order".equalsIgnoreCase(body)
+                ? new Date(System.currentTimeMillis()).toString() : "BAD ORDER";
+        currentTime = currentTime + System.getProperty("line.separator");
+        ByteBuf resp = Unpooled.copiedBuffer(currentTime.getBytes());
+        ctx.write(resp);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) {
+        ctx.flush();
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        ctx.close();
+    }
+}
