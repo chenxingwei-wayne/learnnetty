2868baa02b9d5b6255a0094a7d53a32e8a9be4e2
chenxingwei <shengwei8804@gmail.com>
2024-01-10 23:36:14 +0800

    :sparkles: 补充一个原始的echoserver用例

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # learnnetty
 
 This repo is used to record the test code in learning netty.
+
+~~~text
+项目在一开始初始化时就应该准备好.gitignore(根据你项目的类型网上会有对应的模板可以借鉴)，可以免去后续处理的烦恼。
+~~~
+
---
+++ src/main/java/com/phei/netty/echoserver/EchoServerHandler.java
@@ -0,0 +1,33 @@
+package com.phei.netty.echoserver;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelFutureListener;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.util.CharsetUtil;
+
+/**
+ * 尽管ChannelInboundHandlerAdapter禁用了我们还是使用ChannelInboundHandlerAdapter体会一下netty的最基本的用例。
+ */
+public class EchoServerHandler extends ChannelInboundHandlerAdapter {
+    int counter = 0;
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) {
+        ByteBuf in = (ByteBuf) msg;
+        System.out.println("Server received: " + in.toString(CharsetUtil.UTF_8));
+        ctx.write(in);
+    }
+
+    @Override
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+        // 可以自动关闭channelFuture的channel
+        ctx.writeAndFlush(Unpooled.EMPTY_BUFFER).addListener(ChannelFutureListener.CLOSE);
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+        cause.printStackTrace();;
+        ctx.close();
+    }
+}
